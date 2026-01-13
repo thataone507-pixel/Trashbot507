52b80b8982b743838c3d06181760c329257eeb6c
Thataone507 <thataone507@gmail.com>
2026-01-13 08:53:30 +0200

    Update doNotDelete string in help command

--- scripts/cmds/00_help.js
+++ scripts/cmds/00_help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "🇵🇭|𝗬𝗔𝗭𝗞𝗬 𝗔𝗜";
+const doNotDelete = "🇵🕸😌|TRASH AI ";
 /** 
 * @author NTKhang
 * @author: do not delete it
